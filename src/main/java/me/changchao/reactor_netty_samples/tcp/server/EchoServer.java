ff4db82398200206798c48ff9e9c73e437e33e67
chang-chao <chang-chao@hotmail.com>
2019-03-27 21:21:10 +0900

    Remove handler for bug.

--- src/main/java/me/changchao/reactor_netty_samples/tcp/server/EchoServer.java
+++ src/main/java/me/changchao/reactor_netty_samples/tcp/server/EchoServer.java
@@ -3,8 +3,6 @@ package me.changchao.reactor_netty_samples.tcp.server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.netty.channel.ChannelDuplexHandler;
-import io.netty.channel.ChannelHandlerContext;
 import reactor.netty.DisposableServer;
 import reactor.netty.NettyPipeline.SendOptions;
 import reactor.netty.tcp.TcpServer;
@@ -12,37 +10,15 @@ import reactor.netty.tcp.TcpServer;
 public class EchoServer {
 	final static Logger logger = LoggerFactory.getLogger(EchoServer.class);
 
-	static class EchoServerConnectionHandler extends ChannelDuplexHandler {
-
-		@Override
-		public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
-			logger.debug("channelUnregistered");
-			super.channelUnregistered(ctx);
-		}
-
-		@Override
-		public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
-			logger.debug("channelRegistered");
-			super.channelRegistered(ctx);
-		}
-
-		@Override
-		public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-			logger.debug("channelInactive");
-			super.channelInactive(ctx);
-		}
-
-	}
-
 	public static void main(String[] args) {
 		int listenPort = 9999;
 
 		DisposableServer server = TcpServer.create().port(listenPort).doOnConnection(connection -> {
-			// only when client is connected
+			// only when client is connected, this is OK
 			logger.debug("connection:" + connection);
-			connection.addHandlerLast(new EchoServerConnectionHandler());
 		}).wiretap(true).observe((connection, newState) -> {
-			// only when connected and configured, but no disconnected
+			// only called when connection is connected and configured, but not disconnected
+			// this is a BUG ?
 			logger.debug("connection:" + connection + ", newState=" + newState);
 		}).handle(
 				(inbound, outbound) -> outbound.options(SendOptions::flushOnEach)
