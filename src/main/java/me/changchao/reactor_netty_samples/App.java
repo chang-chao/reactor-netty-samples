c73ac5715db4e0988bc10e7d838d3b55e9a79880
chao chang <chang-chao@hotmail.com>
2019-03-27 16:11:42 +0900

    echo server

---
+++ src/main/java/me/changchao/reactor_netty_samples/App.java
@@ -0,0 +1,13 @@
+package me.changchao.reactor_netty_samples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/me/changchao/reactor_netty_samples/tcp/server/EchoServer.java
@@ -0,0 +1,24 @@
+package me.changchao.reactor_netty_samples.tcp.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import reactor.netty.DisposableServer;
+import reactor.netty.NettyPipeline.SendOptions;
+import reactor.netty.tcp.TcpServer;
+
+public class EchoServer {
+	final static Logger logger = LoggerFactory.getLogger(EchoServer.class);
+
+	public static void main(String[] args) {
+		int listenPort = 9999;
+
+		DisposableServer server = TcpServer
+				.create().port(listenPort).wiretap(true).handle((inbound, outbound) -> outbound
+						.options(SendOptions::flushOnEach).sendString(inbound.receive().asString()).neverComplete())
+				.bindNow();
+
+		server.onDispose().block();
+	}
+
+}
